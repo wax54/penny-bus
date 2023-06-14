942af4932aa3b4fbfb651ca526f41d399e6b6045
sam <sam.crewe-sullam@maharishi.foundation>
2023-06-13 20:23:37 -0700

    fixed type error

--- constants/nav.tsx
+++ constants/nav.tsx
@@ -3,20 +3,10 @@ export const nav = [
   {
     text: "Home",
     href: "/",
-    content: (
-      <div id='content' style={{color: 'white', textAlign:'center'}}>
-        <h3>Our Mission Statement</h3>
-        <p>YAdaYada</p>
-        <h3>Who Are We</h3>
-        <p>YAdaYada</p>
-
-      </div>
-    ),
   },
   {
     text: "Bus Tour",
     href: "tour",
-    content: <div>Unfinished</div>,
   },
   {
     text: "Blog",
@@ -26,30 +16,25 @@ export const nav = [
   {
     text: "Videos",
     href: "videos",
-    content: <div>Unfinished</div>,
   },
 
   {
     text: "FAQ",
     href: "faq",
-    content: <div>Unfinished</div>,
   },
 
   {
     text: "Reviews",
     href: "reviews",
-    content: <div>Unfinished</div>,
   },
 
   {
     text: "Tips + Tricks",
     href: "tips",
-    content: <div>Unfinished</div>,
   },
 
   {
     text: "Photos",
     href: "photos",
-    content: <div>Unfinished</div>,
   },
 ] as const;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,22 @@ import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { nav } from "../constants/nav";
 import { Layout } from "../components/Layout";
 
-export default function Home({
-}: InferGetStaticPropsType<GetStaticProps>) {
-  const { content } = nav.find(({ href }) => href === "/") ?? {};
+export default function Home({}: InferGetStaticPropsType<GetStaticProps>) {
 
   return (
     <Layout nav={nav}>
-      <div id="content">{content}</div>
+      <div id="content">
+        <div id="content" style={{ color: "white", textAlign: "center" }}>
+          <h3>Our Mission Statement</h3>
+          <p>YAdaYada</p>
+          <h3>Who Are We</h3>
+          <p>YAdaYada</p>
+        </div>
+      </div>
     </Layout>
   );
 }
 
 export function getStaticProps() {
-  return { props: {  } };
+  return { props: {} };
 }
