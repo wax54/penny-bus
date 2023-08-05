d07ef06f5418f642e946653c32480ea3beb747ff
sam <sam.crewe-sullam@maharishi.foundation>
2023-08-05 16:15:26 -0700

    Fixed deploy errors

--- pages/admin/blog/index.tsx
+++ pages/admin/blog/index.tsx
@@ -7,48 +7,9 @@ export const getStaticProps = publicBlogPage.getStaticProps;
 export const UpdateBlog = ({
   blogs,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [showCreator, setShowCreator] = useState(false);
-  const [newSlug, setNewSlug] = useState("");
-  const [newBody, setNewBody] = useState("");
-  const [loading, setLoading] = useState(false);
-  const makeNewPost = (blog: { slugId: string; body: string }) => {
-    console.log(blog);
-    setLoading(true);
-    BlogApi.update(blog.slugId, blog.body)
-      .then((resp) => {
-        setLoading(false);
-        // blogRef.current = { ...blogRef.current, ...updatedBlog };
-      })
-      .catch((e) => setLoading(false));
-  };
   return (
     <div className="bg-offWhite">
       <a href={`./blog/${NEW_BLOG_SLUG}`}>New post</a>
-      <div className={showCreator ? "" : "hidden"}>
-        <input
-          value={newSlug}
-          onChange={(evt) => {
-            const { value } = evt.target;
-            setNewSlug(value.replace(/\s/g, ""));
-          }}
-        />
-
-        <textarea
-          name="body"
-          className=" my-4 p-4 w-full h-full"
-          value={newBody}
-          onChange={(evt) => {
-            const { value } = evt.target;
-            setNewBody(value);
-          }}
-        />
-        <button
-          disabled={loading || !newSlug || !newBody}
-          onClick={() => makeNewPost({ slugId: newSlug, body: newBody })}
-        >
-          {loading ? "Loading" : "Create!"}
-        </button>
-      </div>
       {blogs.map(({ key, blog }) => {
         return (
           <a className="block my-5 px-2" href={`./blog/` + key} key={key}>
