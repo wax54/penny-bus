bcd21c715b3ddb50578184974ab4b9c31a3a0046
sam <sam.crewe-sullam@maharishi.foundation>
2023-08-04 22:47:43 -0700

    updated nav

--- constants/nav.tsx
+++ constants/nav.tsx
@@ -1,43 +1,41 @@
-export type NavItem = { href: string; text: string, isActive?: boolean };
+export type NavItem = { href: string; text: string; isActive?: boolean };
 export const nav = [
   {
     text: "Home",
     href: "/home",
-    isActive: true
+    isActive: true,
   },
   {
-    text: "Bus Tour",
-    href: "/tour",
-    isActive: false
-  },
-  {
-    text: "Blog",
+    text: "Travel",
     href: "/blog",
-    isActive: true
+    isActive: true,
   },
-
   {
-    text: "Videos",
+    text: "Gallery",
     href: "/videos",
+    isActive: true,
   },
-
   {
-    text: "FAQ",
+    text: "Lifestyle",
     href: "/faq",
+    isActive: true,
   },
-
+  {
+    text: "Shop",
+    href: "/shop",
+    isActive: true,
+  },
+  {
+    text: "Snapshots",
+    href: "/photos",
+    isActive: false,
+  },
   {
     text: "Reviews",
     href: "/reviews",
   },
-
   {
-    text: "Tips + Tricks",
+    text: "",
     href: "/tips",
-  },
-
-  {
-    text: "Photos",
-    href: "/photos",
   },
 ] as NavItem[];
