11cc9c79226c68be3ce3692b2dda9c93f137d6e5
sam <sam.crewe-sullam@maharishi.foundation>
2023-08-06 01:06:51 -0700

    Added Image Slide show

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,6 +1,45 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { IMAGE_PATH, SITE_URL } from "../../config";
+import { useEffect, useState } from "react";
 
+const ImageSlideShow = (
+  props: {
+    urls: string[];
+  } & JSX.IntrinsicElements["img"]
+) => {
+  const [idx, setIdx] = useState(0);
+  useEffect(() => {
+    setIdx((currIdx) => (currIdx + 1 < props.urls.length ? currIdx + 1 : 0));
+    const id = setInterval(
+      () =>
+        setIdx((currIdx) =>
+          currIdx + 1 < props.urls.length ? currIdx + 1 : 0
+        ),
+      6000
+    );
+    return () => clearInterval(id);
+  }, [props.urls.length]);
+  return (
+    <div className={props.className + " relative "}>
+      {props.urls.map((url, currIdx, arr) => (
+        <img
+          key={url}
+          {...props}
+          src={url}
+          className={
+            props.className +
+            ` absolute  left-0 top-0 ` +
+            (idx === currIdx
+              ? " animate-fade-in z-10 "
+              : idx === currIdx + 1 || (idx === 0 && currIdx === arr.length - 1)
+              ? " animate-fade-out z-0 "
+              : " opacity-0 ")
+          }
+        />
+      ))}
+    </div>
+  );
+};
 export default function Blog({
   inventory,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
@@ -8,10 +47,12 @@ export default function Blog({
     <div key={item.title}>
       {" "}
       <a href={item.link}>
-        <span>{item.title}</span>
-       
+        <h1 className="text-center motion-safe:animate-pulse">{item.title}</h1>
+        <ImageSlideShow
+          urls={item.images}
+          className="max-w-screen sm:max-w-[50vw] sm:max-h-[50vh] m-auto "
+        />
       </a>
-      {item.images}
     </div>
   ));
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,39 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // // Or if using `src` directory:
     // "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'textPrimary': '#000000',
-        'textSecondary': '#FFFFFF',
-        'textAccent': '#FFFF00',
-        'primary': '#EEFFEE',
-        'secondary': '#EE9999',
-        'accent': '#99EE99',
-        'secondaryAccent': '#EEFFEE',
-        'offWhite': 'rgba(0,0,0,0.3)'
+        textPrimary: "#000000",
+        textSecondary: "#FFFFFF",
+        textAccent: "#FFFF00",
+        primary: "#EEFFEE",
+        secondary: "#EE9999",
+        accent: "#99EE99",
+        secondaryAccent: "#EEFFEE",
+        offWhite: "rgba(0,0,0,0.3)",
+      },
+
+      keyframes: {
+        "fade-out": {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-out": "fade-out 2s linear",
+        "fade-in": "fade-in 2s linear",
       },
     },
   },
   plugins: [],
-}
-
+};
