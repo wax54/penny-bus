6a5886a819c81af19c1c845fa0dbf5f8caa7a300
sam <sam.crewe-sullam@maharishi.foundation>
2023-08-09 20:47:07 -0700

    Fix build error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { NavItem } from "../constants/nav";
 import { StylesType } from "../constants/styles";
 import { Nav } from "./Nav";
@@ -13,12 +14,12 @@ export const Header = ({
 }) => {
   return (
     <div style={styles.header} className="p-10">
-      <a href="/">
+      <Link href="/">
         <h1 id="page-title" style={styles.title} className="m-5">
           <em>The Penny Bus Project</em>
           {admin ? <strong>ADMIN EDITION</strong> : null}
         </h1>
-      </a>
+      </Link>
       <Nav nav={nav} styles={styles.nav} />
     </div>
   );
