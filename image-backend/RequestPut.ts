74b6d448fe76a20f495d844627301db272e20ec0
sam <sam.crewe-sullam@maharishi.foundation>
2023-10-11 20:38:41 -0600

    image upload fix

--- image-backend/RequestPut.ts
+++ image-backend/RequestPut.ts
@@ -3,7 +3,9 @@ import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 import { ERRORS } from "./Errors";
 import config from "./config";
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+  signatureVersion: "v4",
+});
 
 const CALLER_BASE_FOLDER = "pennybusproject.com";
 const IMAGE_FOLDER = "images";
@@ -40,16 +42,19 @@ export const handler = async (
       type: "image/png" | "image/jpeg";
     };
     const { type, name } = request;
-
-    const storedName =
-      name + "." + new Date().getTime() + "." + name.split(".")[-1];
+    const parts = name.split(".");
+    const storedName = [
+      ...parts.slice(0, -1),
+      new Date().getTime(),
+      ...parts.slice(-1),
+    ].join(".");
     const url = s3.getSignedUrl("putObject", {
       Bucket: config.IMAGE_BUCKET,
       Key: [CALLER_BASE_FOLDER, IMAGE_FOLDER, storedName].join("/"),
       ContentType: type,
       Expires: 60 * 60, // 60s * 60m = 1h
     });
-    return makeResponse(200, { url });
+    return makeResponse(200, { url, storedName, type });
   } catch (e: any) {
     return makeResponse(500, { error: e.message, fullError: e });
   }
